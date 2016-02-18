7f6a53312488a38f203c33c5978f959039d96ff9
Hack Reactor Students <accounts@hackreactor.com>
2016-02-17 17:34:14 -0800

    stopping point on to do list

--- gobbler/gobbler.js
+++ gobbler/gobbler.js
@@ -1,23 +1,62 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: [
+      { text: "This is task 1" },
+      { text: "This is task 2" },
+      { text: "This is task 3" }
+    ]
+  });
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+      return Tasks.find({});
     }
   });
+  
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+      // Get value from form element
+      var text = event.target.text.value;
+
+      // Insert a task into the collection
+      Meteor.call('addTask', text);
+      // Tasks.insert({
+      //   text: text,
+      //   createdAt: new Date() // current time
+      // });
+
+      // Clear form
+      event.target.text.value = "";
     }
   });
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+Tasks = new Mongo.Collection("tasks");
+
+Meteor.methods({
+  addTask: function (text) {
+    console.log('add task tset');
+    // Make sure the user is logged in before inserting a task
+    // if (! Meteor.userId()) {
+    //   throw new Meteor.Error("not-authorized");
+    // }
+ 
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function (taskId) {
+    Tasks.remove(taskId);
+  },
+  setChecked: function (taskId, setChecked) {
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+  }
+});
