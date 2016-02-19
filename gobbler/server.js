dad065b832bc69612256f9b61576756171b835cf
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-02-18 16:02:08 -0800

    Add routes for userSettings and notifications

--- gobbler/server.js
+++ gobbler/server.js
@@ -4,3 +4,17 @@ FlowRouter.route('/', {
     BlazeLayout.render('app_body', {header: 'header', main: 'main', footer: 'footer'});
   }
 });
+
+FlowRouter.route('/userSettings', {
+  action: function(params, queryParams) {
+    console.log("Yeah! We are on the index:");
+    BlazeLayout.render('app_body', {header: 'header', main: 'userSettingsView', footer: 'footer'});
+  }
+});
+
+FlowRouter.route('/notifications', {
+  action: function(params, queryParams) {
+    console.log("Yeah! We are on the index:");
+    BlazeLayout.render('app_body', {header: 'header', main: 'notificationsView', footer: 'footer'});
+  }
+});
