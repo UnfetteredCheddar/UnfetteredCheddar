7186a82eeece60c6736755cb18073f3687001140
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-02-18 16:41:05 -0800

    Group routes into logged in and public

--- gobbler/server.js
+++ gobbler/server.js
@@ -1,20 +1,43 @@
-FlowRouter.route('/', {
-  action: function(params, queryParams) {
-    console.log("Yeah! We are on the index:");
+//Public routes:
+exposed = FlowRouter.group({
+  prefix:'/front'
+});
+
+exposed.route('/', {
+  name:'front',
+  action: function () {
+    BlazeLayout.render('app_body', {header: 'header', main: 'main', footer: 'footer'});
+  }
+})
+
+//Logged-in routes:
+loggedIn = FlowRouter.group({
+  triggersEnter: [ function () {
+    if(!Meteor.userId()) {
+      route = FlowRouter.current();
+      if(route.route.name != 'front') {
+       Session.set('redirectAfterLogin', route.path);
+      }
+      FlowRouter.go('front');
+    }
+  }]
+})
+
+
+loggedIn.route('/', {
+  action: function () {
     BlazeLayout.render('app_body', {header: 'header', main: 'main', footer: 'footer'});
   }
 });
 
-FlowRouter.route('/userSettings', {
-  action: function(params, queryParams) {
-    console.log("Yeah! We are on the index:");
+loggedIn.route('/userSettings', {
+  action: function () {
     BlazeLayout.render('app_body', {header: 'header', main: 'userSettings', footer: 'footer'});
   }
 });
 
-FlowRouter.route('/notifications', {
-  action: function(params, queryParams) {
-    console.log("Yeah! We are on the index:");
+loggedIn.route('/notifications', {
+  action: function () {
     BlazeLayout.render('app_body', {header: 'header', main: 'notificationsView', footer: 'footer'});
   }
 });
