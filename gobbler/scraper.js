b40e8abf775b65995c6dfed1f99139cc215ae8ae
Alexxis Johnson <alexxis.r.p.johnson@gmail.com>
2016-02-22 13:08:11 -0800

    Remove unnecessary node modules

--- gobbler/scraper.js
+++ gobbler/scraper.js
@@ -3,6 +3,7 @@ if (Meteor.isServer) {
     scrapePage: function ( url ) {
       var webpage = Scrape.website(url);
       var webpageText = webpage.text;
+      console.log( webpageText );
     }
   });
 }
