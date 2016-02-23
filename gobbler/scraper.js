0270d954e45163caefa599ee12eb25ed15d43374
Alexxis Johnson <alexxis.r.p.johnson@gmail.com>
2016-02-23 11:24:26 -0800

    Only check update for single giblet and get all html instead of just the text

--- gobbler/scraper.js
+++ gobbler/scraper.js
@@ -1,21 +1,18 @@
 if (Meteor.isServer) {
   Meteor.methods({
-    scrapePage: function ( url ) {
-      var webpage = Scrape.website(url);
-      var webpageText = webpage.text;
+    scrapePage: function ( url, gibletID ) {
+      var webpage = Scrape.url(url);
+      var webpageText = webpage.toString();
+      // var webpageText = webpage.text + webpage.references;
       var hash = Meteor.call('hashText', webpageText);
-      Meteor.call('checkPageUpdates', url, hash, webpageText);
+      Meteor.call('checkPageUpdates', gibletID, hash, webpageText);
     },
-    checkPageUpdates: function( url, hash, pageText ) {
-      var giblets = Giblets.find({url: url});
-      giblets.forEach( function( giblet ) {
-        if ( giblet.hash !== hash ) {
-          if ( giblet.fullText !== pageText ) {
-            Meteor.call('updateSingleGiblet', giblet._id, hash, pageText);
-            console.log(' updated giblet: ', Giblets.find({_id: giblet._id}).fetch());
-          }
-        }
-      });
+    checkPageUpdates: function( gibletID, hash, pageText ) {
+      var giblet = Giblets.findOne({_id: gibletID});
+      if ( giblet.hash !== hash ) {
+        Meteor.call('updateSingleGiblet', giblet._id, hash, pageText);
+        console.log(' updated giblet: ', Giblets.find({_id: giblet._id}).fetch());
+      }
     },
     updateSingleGiblet: function( id, hash, pageText ) {
       Giblets.update({_id: id}, 
