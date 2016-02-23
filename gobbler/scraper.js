00bc9d1d1599456be3e5484ed636de22eee5e910
Alexxis Johnson <alexxis.r.p.johnson@gmail.com>
2016-02-22 18:03:36 -0800

    Only update database if it's out of date

--- gobbler/scraper.js
+++ gobbler/scraper.js
@@ -3,13 +3,20 @@ if (Meteor.isServer) {
     scrapePage: function ( url ) {
       var webpage = Scrape.website(url);
       var webpageText = webpage.text;
-      var hash = Meteor.call( 'hashText', webpageText );
-      Meteor.call('updateGiblets', url, hash, webpageText );
-      console.log(' updated giblets: ', Giblets.find({url: url}).fetch());
+      var hash = Meteor.call('hashText', webpageText);
+      var giblets = Giblets.find({url: url});
+
+      giblets.forEach( function( giblet ) {
+        if ( giblet.hash !== hash ) {
+          if ( giblet.fullText !== webpageText ) {
+            Meteor.call('updateSingleGiblet', giblet._id, hash, webpageText);
+            console.log(' updated giblet: ', Giblets.find({_id: giblet._id}).fetch());
+          }
+        }
+      });
     },
-    // TODO: only call update when necessary because it takes a long time
-    updateGiblets: function( url, hash, pageText ) {
-      Giblets.update({url: url},
+    updateSingleGiblet: function( id, hash, pageText ) {
+      Giblets.update({_id: id}, 
         {$set: {
           hash: hash,
           fullText: pageText
