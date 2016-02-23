1e5e90f4888a65ef4da8f55c1a2d3cf70eb274e1
Brian Fogg <fogg4444@gmail.com>
2016-02-22 18:38:14 -0800

    implement adding empty/undefined entries to giblet url array

--- gobbler/createGiblet.js
+++ gobbler/createGiblet.js
@@ -1,10 +1,24 @@
+if (Meteor.isServer) {
+  Meteor.methods({
+
+    addUrlToArray: function(id) {
+      console.log('add url to array server side!');
+      console.log('Add url to array!');
+      var giblet = Giblets.findOne({'_id': id});
+      console.log(giblet);
+      var urlArray = giblet.url;
+      console.log('Url array: ', urlArray, urlArray.length);
+      urlArray[urlArray.length] = undefined;
+
+      Giblets.update({'_id': id}, {$set: {url: urlArray}});
+      var newGiblet = Giblets.findOne({'_id': id});
+      console.log('Update happens!', newGiblet);   
+    }
+    
+  });
+}
+
 if (Meteor.isClient) {
-
-  var addUrlToArray = function(id) {
-
-    Meteor.call(addUrlToArray, id);
-
-  };
 
   Template.giblet.events({
     'click .addUrlButton': function(event) {
@@ -13,31 +27,12 @@ if (Meteor.isClient) {
       // console.log(event.currentTarget.classList[1]);
       var id = event.target.attributes['mongoid'].value;
       console.log('id from button', id);
-      addUrlToArray(id);
 
+      Meteor.call('addUrlToArray', id);
     },
 
     'click .subtractUrlButton': function(event) {
       console.log('Click Subtract', event)
     }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.methods({
-
-    addUrlToArray: function(id) {
-      console.log('Add url to array!');
-      var giblet = Giblets.findOne({'_id': id});
-      console.log(giblet);
-      var urlArray = giblet.url;
-      console.log('Url array: ', urlArray, urlArray.length);
-      urlArray[urlArray.length] = 'test';
-
-      Giblets.update({'_id': id}, {$set: {url: urlArray}});
-      var newGiblet = Giblets.findOne({'_id': id});
-      console.log('Update happens!', newGiblet);      
-    }
-    
   });
 }
