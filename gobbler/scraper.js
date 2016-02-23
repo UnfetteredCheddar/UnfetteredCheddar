c3b78ee128d99e4e1882aa2b68950e9ace6bfba6
Alexxis Johnson <alexxis.r.p.johnson@gmail.com>
2016-02-22 17:16:28 -0800

    Hash webpages and store webpage text and hashed version in giblets

--- gobbler/scraper.js
+++ gobbler/scraper.js
@@ -3,7 +3,20 @@ if (Meteor.isServer) {
     scrapePage: function ( url ) {
       var webpage = Scrape.website(url);
       var webpageText = webpage.text;
-      console.log( webpageText );
+      var hash = Meteor.call( 'hashText', webpageText );
+      Meteor.call('updateGiblets', url, hash, webpageText );
+    },
+    // TODO: only call update when necessary because it takes a long time
+    updateGiblets: function( url, hash, pageText ) {
+      Giblets.update({url: url},
+        {$set: {
+          hash: hash,
+          fullText: pageText
+        }
+      });
+    },
+    hashText: function ( pageText ) {
+      return CryptoJS.SHA1(pageText).toString();
     }
   });
 }
