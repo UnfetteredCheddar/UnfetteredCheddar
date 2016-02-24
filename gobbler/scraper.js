602e80590571542c9d21f9af43665d85251d128c
Alexxis Johnson <alexxis.r.p.johnson@gmail.com>
2016-02-23 20:38:48 -0800

    Add whitespace and clean up comments

--- gobbler/scraper.js
+++ gobbler/scraper.js
@@ -1,6 +1,5 @@
 if (Meteor.isServer) {
   Meteor.methods({
-    // TODO: refactor to handle multiple urls
     scrapePage: function ( urls, gibletID ) {
       urls.forEach( function ( url, urlIndex ) {
         var webpage = Scrape.url(url);
@@ -11,62 +10,76 @@ if (Meteor.isServer) {
         Meteor.call('checkPageUpdates', gibletID, url, hash, webpageText, urlIndex);
       });
     }, 
+
     checkPageUpdates: function( gibletID, url, hash, pageText, urlIndex ) {
       var giblet = Giblets.findOne({_id: gibletID});
       var urlNoDots = regExURL( url );
+
       if ( !giblet.webData[urlNoDots] || giblet.webData[urlNoDots].hash !== hash ) {
         var keywordObj = Meteor.call('findKeywords', giblet._id, pageText);
         var oldKeywords = giblet.keywordCounts || {};
         var notificationKeys = [];
+        
         for (var key in keywordObj) {
           if ( !(key in oldKeywords) || keywordsObj[key] > oldKeywords[key] ) {
             notificationKeys.push(key);
           }
         }
+
         Meteor.call('createNotification', giblet._id, notificationKeys, url, giblet.owner);
         Meteor.call('updateSingleGiblet', giblet._id, urlIndex, url, hash, keywordObj);
       }
     },
+
     updateSingleGiblet: function( id, urlIndex, url, hash, keywordObj ) {
       var gibletUpdates = {
         url: url,
         hash: hash,
         keywordCounts: keywordObj
       };
+
       var giblet = Giblets.findOne({_id: id});
       var gibletWebData = giblet.webData;
       var urlNoDots = regExURL( url );
       gibletWebData[urlNoDots] = gibletUpdates;
+
       Giblets.update({_id: id}, 
         {$set: {
           webData: gibletWebData
         }
       });
     },
+
     hashText: function ( pageText ) {
       return CryptoJS.SHA1(pageText).toString();
     },
+
     findKeywords: function( gibletID, pageText ) {
       var giblet = Giblets.findOne({_id: gibletID});
       var goodTags = Tags.clean( giblet.keywords );
+
       // convert tags to case-insensitive regular expressions
       var tagRegexArr = [];
       goodTags.forEach( function( tag ) {
         tagRegexArr.push( new RegExp(tag, 'gi'));
       });
+
       var foundKeywords = [];
       tagRegexArr.forEach( function( tagRegex ) {
         var matchingArr = pageText.match(tagRegex);
         foundKeywords.push( matchingArr );
       });
+
       var keywords = {}
       foundKeywords.forEach(function(array) {
         if (array) {
           keywords[array[0]] = array.length;
         }
       });
+
       return keywords;
     },
+
     createNotification: function ( gibletID, notificationKeys, url, owner ) {
       Notifications.insert({
         createdAt: new Date(),
