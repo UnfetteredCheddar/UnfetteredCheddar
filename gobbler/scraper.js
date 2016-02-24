e06e1fa16018e0428bf9881029c2c54eac298800
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-02-23 18:20:36 -0800

    Create notifications from scraped pages

--- gobbler/scraper.js
+++ gobbler/scraper.js
@@ -7,19 +7,23 @@ if (Meteor.isServer) {
       var webpageText = $('body').text().replace(/\n/g, ' ').replace(/\t/g, ' ').replace('  ', ' ');
       var hash = Meteor.call('hashText', webpageText);
       Meteor.call('checkPageUpdates', gibletID, hash, webpageText);
-    },
+    }, 
     checkPageUpdates: function( gibletID, hash, pageText ) {
       var giblet = Giblets.findOne({_id: gibletID});
       if ( giblet.hash !== hash ) {
         var keywordObj = Meteor.call('findKeywords', giblet._id, pageText);
-        var oldKeywords = giblet.keywordCounts;
-        if(!(_.isEqual(keywordObj, oldKeywords))) {
-          //if different, create notification          
+        var oldKeywords = giblet.keywordCounts || {};
+        var notificationKeys = [];
+        for (var key in keywordObj) {
+          if (!(key in oldKeywords) || keywordsObj[key] > oldKeywords[key]) {
+            notificationKeys.push(key);
+          }
         }
-        Meteor.call('updateGiblet', giblet._id, hash, keywordObj);
+        Meteor.call('createNotification', giblet._id, notificationKeys, giblet.url, giblet.owner);
+        Meteor.call('updateSingleGiblet', giblet._id, hash, keywordObj);
       }
     },
-    updateGiblet: function( id, hash, keywordObj ) {
+    updateSingleGiblet: function( id, hash, keywordObj ) {
       Giblets.update({_id: id}, 
         {$set: {
           hash: hash,
@@ -47,10 +51,23 @@ if (Meteor.isServer) {
       });
       var keywords = {}
       foundKeywords.forEach(function(array) {
-        keywords[array[0]] = array.length;
+        if (array) {
+          keywords[array[0]] = array.length;
+        }
       });
       console.log(keywords);
       return keywords;
+    },
+    createNotification: function(gibletID, notificationKeys, url, owner) {
+      Notifications.insert({
+        createdAt: new Date(),
+        owner: owner,
+        giblet: gibletID,
+        keywords: notificationKeys,
+        url: url
+        // SMS: giblet.SMS,
+        // email: giblet.email,
+      });
     }
   });
 }
