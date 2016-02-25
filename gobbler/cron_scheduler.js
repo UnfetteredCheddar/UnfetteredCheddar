a2df12c28ccbabe40ff6149b9fab669506665c4c
Brian Fogg <fogg4444@gmail.com>
2016-02-24 19:35:36 -0800

    implement ability to modify url array after giblet creation

--- gobbler/cron_scheduler.js
+++ gobbler/cron_scheduler.js
@@ -2,20 +2,14 @@ if (Meteor.isServer) {
   
   Meteor.methods({
     scheduleGiblet: function(gibletID, frequency) {
-      console.log('Start giblet Timer!', frequency);
       frequency = parseInt(frequency);
-      var giblet = Giblets.findOne({'_id': gibletID});
-      var urlArray = giblet.url;
-      console.log(urlArray);
       SyncedCron.add({
         name: gibletID,
         schedule: function(parser) {
           return parser.recur().every(frequency).minute();
         },
         job: function() {
-          console.log('Get rolling!', urlArray, gibletID);
-          // var urlArr = [url];
-          Meteor.call('runGiblet', urlArray, gibletID);
+          Meteor.call('runGiblet', gibletID);
         }
       });
     },
