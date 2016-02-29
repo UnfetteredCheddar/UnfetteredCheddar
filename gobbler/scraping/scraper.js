b98f926fbfab8411154d106beddf54a227aea66a
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-02-29 11:53:13 -0800

    Send emails, create notifications directory

--- gobbler/scraping/scraper.js
+++ gobbler/scraping/scraper.js
@@ -36,38 +36,7 @@ if (Meteor.isServer) {
           webData: webDataCopy
         }
       });
-    },
-
-    // send user an email when they get a notifcation
-    createNotification: function ( giblet, url, notificationKeys ) {
-      Notifications.insert({
-        createdAt: new Date(),
-        owner: giblet.owner,
-        gibletID: giblet._id,
-        keywords: notificationKeys,
-        url: url
-      });
-      var user = Meteor.users.findOne({_id: giblet.owner});
-      var subject = 'Gobbler alert: Found keywords from ' + giblet.taskname;
-      var text = 'Found keywords ' + notificationKeys.join(', ') + ' at ' + url;
-      var email;
-      if (user.services.facebook) {
-        email = user.services.facebook.email;
-      }
-      if (user.services.google) {
-        email = user.services.google.email;
-      }
-      if (user.chosenEmail) {
-        email = user.chosenEmail;
-      }
-      Email.send({
-        to: email,
-        from: 'GobblerGonnaGobble@gmail.com',
-        subject: subject,
-        text: text
-      });
     }
-
   });
 }
 
